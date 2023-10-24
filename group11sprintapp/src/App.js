dba51bd85eb922050f8292e8f872bbb4364d09c3
mitchell-barkley <138683597+mitchell-barkley@users.noreply.github.com>
2023-10-24 19:26:16 -0230

    Update App.js
    adding background image

--- group11sprintapp/src/App.js
+++ group11sprintapp/src/App.js
@@ -1,10 +1,23 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="background">
+      <h1>Group 11 Gastropub</h1>
+      </div>
+    );
+  }
+}
+export default App;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
